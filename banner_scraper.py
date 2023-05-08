322e4b1ea27cb0ec7e143e8f8c7673d36fd1eae4
Brandon-Jeremy <brandonnader1@gmail.com>
2023-05-08 22:11:44 +0300

    Complete Web scraper, CSV writer added

--- banner_scraper.py
+++ banner_scraper.py
@@ -6,6 +6,32 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time
 
+def write_csv(filename):
+    with open(f"{filename}", 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Select","CRN","Subj","Crse","Sec","Cmp","Cred","Title","Days","Time","Cap","Act","Rem","WL","Cap","WL","Act","WL","Rem","XL","Cap","XL","Act","XL","Rem","Instructor","Date (MM/DD)","Location","Attribute"])
+
+        table = driver.find_element(By.XPATH, "//table[@class='datadisplaytable']")
+        rows = table.find_elements(By.XPATH, "//tr")[2:] 
+        """
+            Inside datadisplaytable in html source code there is another part called
+            captiontext and within that there exists rows.
+            Row 0 is Major name
+            Row 1 is Source ...
+            Row 2 is actual information
+        """ 
+        count = 5
+        for row in rows:
+            if count == 0:
+                cols = row.find_elements(By.XPATH, "./td")
+                row_data = []
+                for col in cols:
+                    row_data.append(col.text.strip()+"\t")
+                if len(row_data) > 0:
+                    writer.writerow(row_data)
+            else:
+                count-=1
+
 username = sys.argv[1]
 password = sys.argv[2]
 
@@ -42,5 +68,7 @@ campus.select_by_visible_text('Byblos')
 btn = driver.find_element(By.CSS_SELECTOR, "input[type='submit'][value='Section Search']")
 btn.click()
 
+filename = input("Enter file to store data ending with .csv:\n")
+write_csv(filename)
 
 driver.close()
